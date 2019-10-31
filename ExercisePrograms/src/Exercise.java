ab6eb7176a64bf52285289fb28d11f6e6d673bcd
Shaisthaafreen48 <54101156+Shaisthaafreen48@users.noreply.github.com>
2019-10-31 14:54:11 +0530

    Update Exercise.java

--- ExercisePrograms/src/Exercise.java
+++ ExercisePrograms/src/Exercise.java
@@ -14,6 +14,7 @@ public class Exercise {
 	System.out.println(Exercise.i);  //calling static variable i using class name
 	Exercise e1=new Exercise(); // by creating an object we can access non static member 
 	e1.Ex2(); //accessing non static member(method in this case)  inside a static context
+	System.out.print("hi");
 		
 	
 		
